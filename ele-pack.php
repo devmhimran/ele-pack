9dc330268ed34ea41b313b327015f0f04d4400b0
Imran <mahmud.bubt.150@gmail.com>
2021-09-28 15:16:19 +0600

    Update Version

--- ele-pack.php
+++ ele-pack.php
@@ -3,7 +3,7 @@
 Plugin Name: Ele Pack Addons
 Plugin URI:
 Description: Boilerplate for creating Elementor Extensions
-Version: 1.0.7
+Version: 1.0.8
 Author: MH Imran
 Author URI: https://hasin.me
 License: GPLv2 or later
